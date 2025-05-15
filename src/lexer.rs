11bbe4d3bfa004a6b3891200a42b2020736ab57f
dinutting <dinutting@outlook.com>
2025-05-14 23:07:02 -0400

    Tweaks and documentation

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,18 +1,7 @@
 #![allow(dead_code)]
-/// An enumeration used for tracking the different types of 
-/// tokens in the lexer. 
-/// - When adding, or changing, the TokenTypes,
-/// Make sure to update the generate_tmakers function.
-/// 
-/// I wanted to approach this as constant values, but the Regex
-/// crate doesn't work for const functions. lazy_regex might work.
-//#[derive(Copy, Clone, PartialEq)]
 
-//"void main() { return; }"
-
-#[derive(Debug)]
-#[derive(PartialEq)]
-pub enum TokenTypes {
+#[derive(Debug, PartialEq)]
+pub enum TokenType {
     Whitespace(usize),
     Constant(isize),
     IntKeyword,
@@ -27,37 +16,53 @@ pub enum TokenTypes {
     Empty
 }
 
-impl std::fmt::Display for TokenTypes {
+impl std::fmt::Display for TokenType {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            TokenTypes::Whitespace(_i) => write!(f, "Whitespace"),
-            TokenTypes::Constant(i) => write!(f, "Constant: {}", i),
-            TokenTypes::IntKeyword => write!(f, "IntKeyword"),
-            TokenTypes::VoidKeyword => write!(f, "VoidKeyword"),
-            TokenTypes::ReturnKeyword => write!(f, "ReturnKeyword"),
-            TokenTypes::Identifier(s) => write!(f, "Identifier: {}", s),
-            TokenTypes::OpenParen => write!(f, "OpenParen"),
-            TokenTypes::CloseParen => write!(f, "CloseParen"),
-            TokenTypes::OpenBrace => write!(f, "OpenBrace"),
-            TokenTypes::CloseBrace => write!(f, "CloseBrace"),
-            TokenTypes::Semicolon => write!(f, "Semicolon"),
-            TokenTypes::Empty => write!(f, "Empty"),
+            TokenType::Whitespace(_i) => write!(f, "Whitespace"),
+            TokenType::Constant(i) => write!(f, "Constant: {}", i),
+            TokenType::IntKeyword => write!(f, "IntKeyword"),
+            TokenType::VoidKeyword => write!(f, "VoidKeyword"),
+            TokenType::ReturnKeyword => write!(f, "ReturnKeyword"),
+            TokenType::Identifier(s) => write!(f, "Identifier: {}", s),
+            TokenType::OpenParen => write!(f, "OpenParen"),
+            TokenType::CloseParen => write!(f, "CloseParen"),
+            TokenType::OpenBrace => write!(f, "OpenBrace"),
+            TokenType::CloseBrace => write!(f, "CloseBrace"),
+            TokenType::Semicolon => write!(f, "Semicolon"),
+            TokenType::Empty => write!(f, "Empty"),
             //_ => write!(f, "Weird TokenType Found!"),
         }
     }
 }
 
-fn mad_scan(source: &str) -> TokenTypes {
+/// Receives a reference to a string and examines the beginning of it for a valid token.
+/// If one is found, it returns the corresponding TokenType
+/// If one is not found, it provides a TokenType::Empty
+/// 
+/// # Example
+/// ```
+/// let len = source.len();
+/// let mut pointer: usize = 0;
+/// let mut tokens: Vec<TokenType> = Vec::new();
+///
+/// while pointer<len {
+///     match mad_scan(&source[pointer..len]) {
+///          TokenType::Whitespace(x) => {pointer+=x; tokens.push(TokenType::Whitespace(x))},
+/// ...
+/// ...
+/// ```
+fn mad_scan(source: &str) -> TokenType {
 
     // Grab the first character in source. This might be a dumb way of doing this, instead of just grabbing a slice
     // but the iterator does let me use the take_while, which is handy.
     // I would like to explore if there's ways to match on variable length slices
     match source.chars().next() {
 
-        None => TokenTypes::Empty, // This should trigger no more scanning
+        None => TokenType::Empty, // This should trigger no more scanning
 
         // if the beginning of the string is whitespace, count all the whitespace until a non whitespace character shows up.
-        Some(c) if c.is_whitespace() => TokenTypes::Whitespace(source.chars()
+        Some(c) if c.is_whitespace() => TokenType::Whitespace(source.chars()
                                                                            .take_while(|c| c.is_whitespace())
                                                                            .count() as usize),
         
@@ -65,21 +70,21 @@ fn mad_scan(source: &str) -> TokenTypes {
         Some(c) if c.is_alphabetic() => { 
             match c {
                 // this is a weird way of doing this, but it works
-                'v' if &source[0..5] == "void " => TokenTypes::VoidKeyword,
-                'i' if &source[0..4] == "int " => TokenTypes::IntKeyword,
-                'r' if &source[0..7] == "return " => TokenTypes::ReturnKeyword,
-                'r' if &source[0..7] == "return;" => TokenTypes::ReturnKeyword,
+                'v' if &source[0..5] == "void " => TokenType::VoidKeyword,
+                'i' if &source[0..4] == "int " => TokenType::IntKeyword,
+                'r' if &source[0..7] == "return " => TokenType::ReturnKeyword,
+                'r' if &source[0..7] == "return;" => TokenType::ReturnKeyword,
                 // if it's not a keyword, assume it's an identifier
                 // take all the alphanumeric until a non-alphanumeric is received.
                 // ***TODO, account for dashes and underscores***
-                _   => TokenTypes::Identifier(source.chars()
+                _   => TokenType::Identifier(source.chars()
                                                             .take_while(|c| c.is_alphanumeric())
                                                             .collect::<String>()),
             }
         },
         // if it starts with a number, assume it's a numeric constant.
         // **TODO account for negative and decimal**
-        Some(c) if c.is_numeric() => TokenTypes::Constant(source.chars()
+        Some(c) if c.is_numeric() => TokenType::Constant(source.chars()
                                                                        .take_while(|c| c.is_numeric())
                                                                        .collect::<String>()
                                                                        // **** TODO **** Address this unwrap
@@ -88,42 +93,58 @@ fn mad_scan(source: &str) -> TokenTypes {
         // if it isn't numeric, whitespace, or alphabetical, it better be punctuation.
         Some(c) if c.is_ascii_punctuation() => {
             match c {
-                '{' => TokenTypes::OpenBrace,
-                '(' => TokenTypes::OpenParen,
-                '}' => TokenTypes::CloseBrace,
-                ')' => TokenTypes::CloseParen,
-                ';' => TokenTypes::Semicolon,
-                _   => TokenTypes::Empty,
+                '{' => TokenType::OpenBrace,
+                '(' => TokenType::OpenParen,
+                '}' => TokenType::CloseBrace,
+                ')' => TokenType::CloseParen,
+                ';' => TokenType::Semicolon,
+                _   => TokenType::Empty,
             }
         },
-        Some(_) => TokenTypes::Empty,
+        Some(_) => TokenType::Empty,
     } 
     }
 
-fn lexer(source: &str) -> Vec<TokenTypes> {
+/// Lexes the target string and returns a Vector of TokenType
+/// The utility of this is tightly coupled to the parameters attached to the varying TokenTypes.
+/// as the lexer needs to know by how many characters it should move the pointer as each token is discovered.
+/// Fixed width keywords such as `int` and `void` have fixed length pointer moves.
+/// Where as variable length tokens such as whitespace and identifiers calculate how far the pointer should
+/// move based on the length of the attached string.
+/// Constant is an awkward tokentype, as it should ideally be stored in it's target token type, but has to temporarily
+/// be converted to a string to find the length. This could be problematic later, and should probably be changed to a string
+/// and then let the parser grab the number.
+/// # Example
+/// ```
+/// let tokens: Vec<TokenType> = lexer("int main() { return 365; }");
+/// for token in tokens {
+///     println!("{}", token);
+/// }
+/// ```
+fn lexer(source: &str) -> Vec<TokenType> {
     let len = source.len(); // **TODO DEAL WITH UNWRAPS**
     let mut pointer: usize = 0;
-    let mut tokens: Vec<TokenTypes> = Vec::new();
+    let mut tokens: Vec<TokenType> = Vec::new();
 
     while pointer<len {
         match mad_scan(&source[pointer..len]) {
-            TokenTypes::Whitespace(x) => {pointer+=x; tokens.push(TokenTypes::Whitespace(x))},
-            TokenTypes::Identifier(s) => {pointer+=s.len(); tokens.push(TokenTypes::Identifier(s))},
-            TokenTypes::Constant(i) => {pointer+=i.to_string().len(); tokens.push(TokenTypes::Constant(i))},
+            TokenType::Whitespace(x) => {pointer+=x; tokens.push(TokenType::Whitespace(x))},
+            TokenType::Identifier(s) => {pointer+=s.len(); tokens.push(TokenType::Identifier(s))},
+            TokenType::Constant(i) => {pointer+=i.to_string().len(); tokens.push(TokenType::Constant(i))},
             
-            TokenTypes::IntKeyword => {pointer+=3; tokens.push(TokenTypes::IntKeyword)},
-            TokenTypes::VoidKeyword => {pointer+=4; tokens.push(TokenTypes::VoidKeyword)},
-            TokenTypes::ReturnKeyword => {pointer+=6; tokens.push(TokenTypes::ReturnKeyword)},
+            TokenType::IntKeyword => {pointer+=3; tokens.push(TokenType::IntKeyword)},
+            TokenType::VoidKeyword => {pointer+=4; tokens.push(TokenType::VoidKeyword)},
+            TokenType::ReturnKeyword => {pointer+=6; tokens.push(TokenType::ReturnKeyword)},
             
-            TokenTypes::CloseBrace => {pointer+=1; tokens.push(TokenTypes::CloseBrace)},
-            TokenTypes::OpenBrace => {pointer+=1; tokens.push(TokenTypes::OpenBrace)},
+            TokenType::CloseBrace => {pointer+=1; tokens.push(TokenType::CloseBrace)},
+            TokenType::OpenBrace => {pointer+=1; tokens.push(TokenType::OpenBrace)},
             
-            TokenTypes::CloseParen => {pointer+=1; tokens.push(TokenTypes::CloseParen)},
-            TokenTypes::OpenParen => {pointer+=1; tokens.push(TokenTypes::OpenParen)},
+            TokenType::CloseParen => {pointer+=1; tokens.push(TokenType::CloseParen)},
+            TokenType::OpenParen => {pointer+=1; tokens.push(TokenType::OpenParen)},
             
-            TokenTypes::Semicolon => {pointer+=1; tokens.push(TokenTypes::Semicolon)},
+            TokenType::Semicolon => {pointer+=1; tokens.push(TokenType::Semicolon)},
 
-            TokenTypes::Empty => break,
+            TokenType::Empty => break,
             //_ => break,
         }
     }
@@ -132,7 +153,7 @@ fn lexer(source: &str) -> Vec<TokenTypes> {
 
 #[test]
 fn test_lexer() {
-    let tokens: Vec<TokenTypes> = lexer("int main() { return 365; }");
+    let tokens: Vec<TokenType> = lexer("int main() { return 365; }");
     for token in tokens {
         println!("{}", token);
     }
@@ -162,27 +183,27 @@ mod tests {
     #[test]
     fn mad_scan_number(){
         let source = String::from("365 days");
-        assert_eq!(mad_scan(&source), TokenTypes::Constant(365))
+        assert_eq!(mad_scan(&source), TokenType::Constant(365))
     }
 
     #[test]
     fn mad_scan_whitespace(){
         let source = String::from("      space tab space"); // note tab entered 4 spaces
-        assert_eq!(mad_scan(&source), TokenTypes::Whitespace(6))
+        assert_eq!(mad_scan(&source), TokenType::Whitespace(6))
     }
 
     #[test]
     fn mad_scan_return(){
         let mut source = String::from("return;");
-        assert_eq!(mad_scan(&source), TokenTypes::ReturnKeyword);
+        assert_eq!(mad_scan(&source), TokenType::ReturnKeyword);
         source = String::from("return 32;");
-        assert_eq!(mad_scan(&source), TokenTypes::ReturnKeyword);
+        assert_eq!(mad_scan(&source), TokenType::ReturnKeyword);
     }
 
     #[test]
     fn mad_scan_identifier(){
         let source = String::from("variable123 = 10");
-        assert_eq!(mad_scan(&source), TokenTypes::Identifier(String::from("variable123")))
+        assert_eq!(mad_scan(&source), TokenType::Identifier(String::from("variable123")))
     }
 
     #[test]
